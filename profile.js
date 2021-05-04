53e5fe3e8d8ecf0f250fa2574e73f627f6389e1c
etsuyayoshida <user@etsuya-yoshida.local>
2021-05-04 19:35:56 +0900

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,2 +1,2 @@
-let practice = document.querySelector('.main h1');
-practice.innerHTML = '<h6>こんばんは</h6>';
+let practice = document.querySelector('.main .hello');
+practice.innerHTML = '<h1>こんばんは</h1>';
