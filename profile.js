8cf4efb968dd09994d3256f3a95c7709590ca2ad
etsuyayoshida <user@etsuya-yoshida.local>
2021-05-05 17:57:39 +0900

    Update profile.js

--- profile.js
+++ profile.js
@@ -20,3 +20,11 @@ for (let i = 0; i < results.length; i++) {
 	console.log('index:' + i + 'データ:' + results[i]);
 }
 
+let omikuji = {
+	results: ['大吉','吉','中吉','小吉','凶']
+    getResults: function() {
+    	let results = this.results
+    	return results[Math.floor(Math.random() * results.length)];
+    }
+}
+   
