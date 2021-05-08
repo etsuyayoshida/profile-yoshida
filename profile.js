7e799ab9453ec2055bb59c88fe21e1af2a7e1791
etsuyayoshida <user@etsuya-yoshida.local>
2021-05-08 14:15:49 +0900

    Update profile.js

--- profile.js
+++ profile.js
@@ -20,7 +20,7 @@ console.log(omikuji.getResult());
 let element = document.querySelector('h6');
 element.innerHTML = '<h6>吉田のおみくじ</h6>';
 element.style.color = '#FF0000';
-element.style.left = '80px';
+element.style.marginLeft = '80px';
 // 要素オブジェクトの取得
 let getResult = document.getElementById('getResult');
 let result = document.getElementById('result');
