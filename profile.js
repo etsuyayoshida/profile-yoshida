97e1c598c67fcd4490ff27860afe03d6e6abbf53
etsuyayoshida <user@etsuya-yoshida.local>
2021-05-08 14:04:12 +0900

    Update profile.js

--- profile.js
+++ profile.js
@@ -17,7 +17,7 @@ let omikuji = {
 
 console.log(omikuji.getResult()); 
 
-let element = document.querySelector('.omake');
+let element = document.querySelector('.omake' 'h6');
 element.innerHTML ='<h6>吉田のおみくじ</h6>'
 // 要素オブジェクトの取得
 let getResult = document.getElementById('getResult');
