3847e986133e0c9fbd31a8c19371aef602736e66
etsuyayoshida <user@etsuya-yoshida.local>
2021-05-08 13:26:48 +0900

    Update profile.js

--- profile.js
+++ profile.js
@@ -18,7 +18,7 @@ let omikuji = {
 console.log(omikuji.getResult()); 
 
 let element = document.querySelector('.omake h6');
-element.style.color = '#FF0000';
+element.style.backgroundcolor = '#FF0000';
 
 // 要素オブジェクトの取得
 let getResult = document.getElementById('getResult');
