72df25bfb2a5ec9864b3bdf6d04afd2cf3ac7085
etsuyayoshida <user@etsuya-yoshida.local>
2021-05-09 21:40:08 +0900

    Update profile.js

--- profile.js
+++ profile.js
@@ -14,9 +14,7 @@ mainImage.setAttribute('alt', album[0].msg);
 let mainMsg = document.createElement('p');
 mainMsg.innerText = mainImage.alt;
 
-let mainFlame = document.querySelector('#gallery .mainpic');
-mainFlame.insertBefore(mainImage, null);
-mainFlame.insertBefore(mainMsg, null);
+
 
 
 
